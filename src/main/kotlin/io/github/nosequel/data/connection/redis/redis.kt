b7ceb91f28107e41ff6f686d87069f36c44d5ec4
devrawr <rawrdev@protonmail.com>
2022-01-06 18:49:42 +0100

    add PasswordRedisConnectionPool and PasswordUserRedisConnectionPool

--- src/main/kotlin/io/github/nosequel/data/connection/redis/redis.kt
+++ src/main/kotlin/io/github/nosequel/data/connection/redis/redis.kt
@@ -20,3 +20,38 @@ class NoAuthRedisConnectionPool(
         return this.jedis
     }
 }
+
+class PasswordRedisConnectionPool(
+    hostname: String,
+    port: Int,
+
+    private val password: String
+): RedisConnectionPool()
+{
+    override val jedis = Jedis(hostname, port)
+
+    override fun getConnectionPool(): Jedis
+    {
+        return this.jedis.apply {
+            this.auth(password)
+        }
+    }
+}
+
+class PasswordUserRedisConnectionPool(
+    hostname: String,
+    port: Int,
+
+    private val password: String,
+    private val username: String
+): RedisConnectionPool()
+{
+    override val jedis = Jedis(hostname, port)
+
+    override fun getConnectionPool(): Jedis
+    {
+        return this.jedis.apply {
+            this.auth(username, password)
+        }
+    }
+}
