9e2876f3ab79722da826bf8539926e1bbbeb06d0
devrawr <devrawr@protonmail.com>
2022-03-25 15:27:39 +0100

    add AuthenticatedMongoConnectionPool implementation

--- src/main/kotlin/io/github/nosequel/data/connection/mongo/mongo.kt
+++ src/main/kotlin/io/github/nosequel/data/connection/mongo/mongo.kt
@@ -1,6 +1,8 @@
 package io.github.nosequel.data.connection.mongo
 
 import com.mongodb.MongoClient
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
 import com.mongodb.client.MongoCollection
 import io.github.nosequel.data.connection.ConnectionPool
 import org.bson.Document
@@ -40,5 +42,42 @@ class NoAuthMongoConnectionPool : MongoConnectionPool()
             hostname!!,
             port!!
         )
+    }
+}
+
+class AuthenticatedMongoConnectionPool : MongoConnectionPool()
+{
+    override var databaseName: String? = null
+
+    var hostname: String? = null
+    var port: Int? = null
+
+    var password: String? = null
+    var username: String? = null
+
+    override fun getConnectionPool(): MongoClient
+    {
+        return if (this.password == null)
+        {
+            MongoClient(
+                hostname!!,
+                port!!
+            )
+        } else
+        {
+            MongoClient(
+                ServerAddress(
+                    hostname!!,
+                    port!!
+                ),
+                listOf(
+                    MongoCredential.createCredential(
+                        this.username!!,
+                        this.databaseName!!,
+                        this.password!!.toCharArray()
+                    )
+                )
+            )
+        }
     }
 }
